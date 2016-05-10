ec5b028d95426126324984dc82fb83fe56b26194
Michael Jaison G <gmichaeljaison@gmail.com>
2016-05-09 20:55:41 -0400

    fixing pypi upload

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from distutils.core import setup
 setup(
     name='specularity_removal',
     packages=['specularity_removal'],
-    version='0.1',
+    version='0.1.1',
     description='Implements specular removal in images captured from various view points',
+    long_description=open('README.md').read(),
     author='Michael Jaison Gnanasekar, Shreyas Joshi',
     author_email='gmichaeljaison@gmail.com, shreyasvj25@gmail.com',
     url='https://github.com/gmichaeljaison/specularity-removal',
     download_url='https://github.com/gmichaeljaison/specularity-removal/tarball/0.1',
     keywords=['computer vision', 'image processing', 'specularity-removal',
               'highlights', 'homography'],
-    classifiers=[],
-    requires=['numpy', 'cv2']
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities"
+    ],
+    requires=['numpy', 'cv2'],
+    license='LICENSE'
 )
